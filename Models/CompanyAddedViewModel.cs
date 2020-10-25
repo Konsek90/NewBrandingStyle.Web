6cf1b85d339f91843d24593b24995a193908993e
Konsek90 <48626081+Konsek90@users.noreply.github.com>
2020-10-25 12:12:28 +0100

    Changes

--- Models/CompanyAddedViewModel.cs
+++ Models/CompanyAddedViewModel.cs
@@ -7,11 +7,13 @@ namespace WebApplication.Web.Models
 {
     public class CompanyAddedViewModel: CompanyModel
     {
-        public int NumberOfCharsInName { get; set; }
-
+       
         public int NumberOfCharsInDescription { get; set; }
 
         public bool IsHidden { get; set; }
+
+        public int NumberOfCharsInName { get; set; }
+
         
     }
 }
